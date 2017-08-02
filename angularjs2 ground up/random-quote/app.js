0e9a05d10d830860411bf56b68a5682b5cfdd8d6
gagan0183 <g.deepsingh1@gmail.com>
2017-08-02 11:05:28 +0530

    changes in components

--- angularjs2 ground up/random-quote/app.js
+++ angularjs2 ground up/random-quote/app.js
@@ -1,21 +1,80 @@
 (function () {
-    var AppComp = ng.core.Component({
+    var Component = ng.core.Component;
+    var NgModule = ng.core.NgModule;
+    var BrowserModule = ng.platformBrowser.BrowserModule;
+    var platformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic;
+
+    var RandomQuoteComponent = Component({
+        selector: 'random-quote',
+        template: '<p><em>{{quote.line}}</em> - {{quote.author}}</p>'
+    })
+    .Class({
+        constructor: function() {      
+            var random = Math.floor(Math.random() * quotes.length);
+            this.quote = quotes[random];
+        }
+    });
+
+    var AppComp = Component({
         selector: 'my-app',
-        template: '<p>JoinsAngulars</p>'
+        template: '<p>JoinsAngulars</p>'+
+                    '<random-quote></random-quote>'
     })
     .Class({
         constructor: function() {}
     });
 
-    var AppModule = ng.core.NgModule({
-        imports: [ng.platformBrowser.BrowserModule],
-        declarations: [AppComp],
+    var AppModule = NgModule({
+        imports: [BrowserModule],
+        declarations: [AppComp, RandomQuoteComponent],
         bootstrap: [AppComp]
     })
     .Class({
         constructor: function () {  }
     });
 
-    ng.platformBrowserDynamic.platformBrowserDynamic()
-        .bootstrapModule(AppModule);
+    platformBrowserDynamic().bootstrapModule(AppModule);
+
+    var quotes = [
+    {
+      "line": "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+      "author": "Brian W. Kernighan"
+    },
+    {
+      "line": "Walking on water and developing software from a specification are easy if both are frozen.",
+      "author": "Edward V Berard"
+    },
+    {
+      "line": "It always takes longer than you expect, even when you take into account Hofstadter's Law.",
+      "author": "Hofstadter's Law"
+    },
+    {
+      "line": "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
+      "author": "Rick Osborne"
+    },
+    {
+      "line": "In theory, there is no difference between theory and practice. But, in practice, there is.",
+      "author": "Jan L. A. van de Snepscheut"
+    },
+    {
+      "line": "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+      "author": "Bill Gates"
+    },
+    {
+      "line": "There are 2 hard problems in computer science: cache invalidation, naming things, and off-by-1 errors.",
+      "author": "Leon Bambrick"
+    },
+    {
+      "line": "Nine people can't make a baby in a month.",
+      "author": "Fred Brooks"
+    },
+    {
+      "line": "If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
+      "author": "Edsger Dijkstra"
+    },
+    {
+      "line": "The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.",
+      "author": "Tom Cargill"
+    }
+  ];
 })();
