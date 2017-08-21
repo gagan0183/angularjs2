bb915fdf752ef6c850e86165bdfb0a5b8e38c6c4
gagan0183 <g.deepsingh1@gmail.com>
2017-08-22 01:11:54 +0530

    comp app

--- angular2-ng/angular2-reddits/src/app/app.component.ts
+++ angular2-ng/angular2-reddits/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 1', 'http://angular.io', 13),
+      new Article('Angular 2', 'http://angular.io', 13),
+      new Article('Angular 3', 'http://angular.io', 13)
+    ]
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title : ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 13));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
--- angular2-ng/angular2-reddits/src/app/article/article.component.ts
+++ angular2-ng/angular2-reddits/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -9,22 +10,15 @@ import { Component, OnInit } from '@angular/core';
   }
 })
 export class ArticleComponent implements OnInit {
-  votes: number;
-  title: string;
-  link: string;
-  constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 13;
-  }
+  @Input() article: Article;
 
   votesUp(): boolean {
-    this.votes += 1;
+    this.article.votesUp();
     return false;
   }
 
   votesDown(): boolean {
-    this.votes -= 1;
+    this.article.votesDown();
     return false;
   }
 
