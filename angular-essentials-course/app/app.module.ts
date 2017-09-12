03a55fc181ead384e6b8bb266165332a6d408689
gagan0183 <g.deepsingh1@gmail.com>
2017-09-13 00:42:43 +0530

    added media item compoennt

--- angular-essentials-course/app/app.module.ts
+++ angular-essentials-course/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from  './app.component';
+import { MediaItemComponent } from  './media.component';
+
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations:[
-    AppComponent
+    AppComponent,
+    MediaItemComponent
   ],
   bootstrap: [
     AppComponent
---
+++ angular-essentials-course/app/media.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item',
+  templateUrl: 'app/media.component.html',
+  styleUrls: ['app/media.component.css']
+})
+export class MediaItemComponent {
+
+}
