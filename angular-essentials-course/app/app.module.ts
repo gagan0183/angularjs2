d2c3e81ea744edb3a81ac1ea09a62a6eba4fa19c
gagan0183 <g.deepsingh1@gmail.com>
2017-09-13 10:16:30 +0530

    comp ngFor structuruals

--- angular-essentials-course/app/app.module.ts
+++ angular-essentials-course/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from  './app.component';
 import { MediaItemComponent } from  './media.component';
+import { MediaItemListComponent } from './media-item-list.component';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { MediaItemComponent } from  './media.component';
   ],
   declarations:[
     AppComponent,
-    MediaItemComponent
+    MediaItemComponent,
+    MediaItemListComponent
   ],
   bootstrap: [
     AppComponent
