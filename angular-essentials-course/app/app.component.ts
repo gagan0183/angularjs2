9ac6af2933f42e489eb2ad46505d1e18f6fb7d0e
gagan0183 <g.deepsingh1@gmail.com>
2017-09-13 10:22:13 +0530

    ngClass

--- angular-essentials-course/app/app.component.ts
+++ angular-essentials-course/app/app.component.ts
@@ -6,17 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
-  firstMediaItem = {
-    id: 1,
-    name: 'Firebug',
-    medium: 'Series',
-    category: 'Science fiction',
-    year: 2000,
-    watchedOn: null,
-    isFavorite: true
-  };
 
-  onMediaItemDelete(mediaItem) {
-    console.log(mediaItem);
-  }
 }
