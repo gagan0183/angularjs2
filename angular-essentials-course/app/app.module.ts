a98171eabce06223def1e8a6650a7b71b5346d12
gagan0183 <g.deepsingh1@gmail.com>
2017-09-14 10:42:51 +0530

    template driven form

--- angular-essentials-course/app/app.module.ts
+++ angular-essentials-course/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import { MediaItemFormComponent } from './media-item-form.component';
 import { AppComponent } from  './app.component';
 import { MediaItemComponent } from  './media.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import {FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations:[
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
-    CategoryListPipe
+    CategoryListPipe,
+    MediaItemFormComponent
   ],
   bootstrap: [
     AppComponent
